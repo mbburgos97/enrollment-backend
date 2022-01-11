95a7117095e019d85519fed9a624491ecf26a48a
Mark Joshua Burgos <joshburgos@Marks-MacBook-Pro.local>
2022-01-11 14:42:13 +0800

    EN-1: Commit boilerplate code for enrollment backend service

---
+++ src/main/java/com/mbburgos/enrollmentbackendservice/EnrollmentBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mbburgos.enrollmentbackendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollmentBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrollmentBackendServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbburgos/enrollmentbackendservice/EnrollmentBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbburgos.enrollmentbackendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollmentBackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
