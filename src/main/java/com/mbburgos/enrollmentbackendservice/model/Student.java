48628397ca4f7b0581e8b708c072688d55057c6b
Josh Burgos <mbburgos97@gmail.com>
2022-01-15 16:55:10 +0800

    EN-3: Added cors for allowing all origin to consume response. Changed response format to snake_case instead of camelCase

--- src/main/java/com/mbburgos/enrollmentbackendservice/model/Student.java
+++ src/main/java/com/mbburgos/enrollmentbackendservice/model/Student.java
@@ -1,4 +1,8 @@
 package com.mbburgos.enrollmentbackendservice.model;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record Student(String studentId, String firstName, String middleName, String lastName, String profileImage) {
 }
