eed85360e48fe98580bb00072f6869096f135630
Josh Burgos <mbburgos97@gmail.com>
2022-02-11 13:49:13 +0800

    EN-19: Added Announcement table and adjusted subject_id to id

--- schema/initial_table.sql
+++ schema/initial_table.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS teacher (
 );
 
 CREATE TABLE if not EXISTS subject (
-	subject_id integer primary key,
+	id serial primary key,
 	teacher_id varchar(64) not null,
 	foreign key (teacher_id)
 		references teacher (teacher_id)
@@ -31,4 +31,16 @@ CREATE TABLE if not EXISTS subject_student (
 	student_id varchar not null,
 	foreign key (student_id)
 		references student (student_id)
+);
+
+CREATE TABLE if not EXISTS announcement (
+	id serial primary key,
+	title varchar(64) not null,
+	subject_id integer not null,
+	content varchar(64) null,
+	teacher_id varchar(64) not null,
+	foreign key (teacher_id)
+		references teacher (teacher_id),
+	foreign key (subject_id)
+		references subject (id)
 );
